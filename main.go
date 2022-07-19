8acc25f9c7e6fa7893485d969962605ccf0bf1b9
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-19 13:01:16 +0700

    apply cors config back to default

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"module/infrastructure"
 	"module/rest"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,26 +25,32 @@ func main() {
 	userRepository := repository.NewUserDao()
 	userHandler := handler.NewUserService(*userRepository)
 	// Running App
-	r := gin.New()
-	r.Use(gin.Recovery())
-	r.Use(CorsMiddleware())
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: false,
+	}))
+	// r.Use(gin.Recovery())
+	// r.Use(CorsMiddleware())
 
 	rest.CheckHealth(r)
 	rest.InitUserRoutes(r, userHandler)
 	r.Run(":8000")
 }
 
-func CorsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
+// func CorsMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		c.Header("Access-Control-Allow-Origin", "*")
+// 		c.Header("Access-Control-Allow-Credentials", "true")
+// 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+// 		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	}
-}
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(204)
+// 			return
+// 		}
+// 		c.Next()
+// 	}
+// }
