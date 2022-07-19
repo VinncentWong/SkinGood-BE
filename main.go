4cab2a4b28890e349c974ecdebc4a052e329dd6b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-19 10:09:56 +0700

    change cors config

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"module/infrastructure"
 	"module/rest"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -26,13 +25,25 @@ func main() {
 	userHandler := handler.NewUserService(*userRepository)
 	// Running App
 	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-		AllowHeaders: []string{"*"},
-		AllowMethods: []string{"POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"},
-	}))
+	r.Use(CorsMiddleware())
 
 	rest.CheckHealth(r)
 	rest.InitUserRoutes(r, userHandler)
 	r.Run(":8000")
+}
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
